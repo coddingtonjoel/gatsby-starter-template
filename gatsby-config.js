70739edfd5e4b2c3d89ed136799e510ff0638b61
Joel Coddington <coddingtonjoel@gmail.com>
2020-08-09 15:07:13 -0700

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,50 @@
+module.exports = {
+    siteMetadata: {
+        title: `Template`,
+        description: `Template Description`,
+        author: `Joel Coddington`,
+        // siteUrl: `https://template.com`,
+    },
+    plugins: [
+        `gatsby-plugin-sitemap`,
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-sass`,
+        `gatsby-plugin-netlify`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Template`,
+                short_name: `Template`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+        // {
+        //     resolve: `gatsby-plugin-prefetch-google-fonts`,
+        //     options: {
+        //         fonts: [
+        //             {
+        //                 family: `Work Sans`,
+        //                 variants: [`300`, `800`, `900`],
+        //             },
+        //             {
+        //                 family: `Lato`,
+        //                 variants: [`300`, `700`],
+        //             },
+        //         ],
+        //     },
+        // },
+    ],
+};
